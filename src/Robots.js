7831ad55c1bfe9d7eb52d66c9ca83af0a6cbfc4e
Raul Guirado <raul.guirado95@gmail.com>
2021-01-25 02:22:40 -0800

    added deploy to publish to github pages

---
+++ src/Robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Raul Guirado',
+        username: 'RaulGuirado',
+        email: 'raul.guirado95@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'John Doe',
+        username: 'John123',
+        email: 'JohhnyDoe@email.com'
+    },
+    {
+        id: 3,
+        name: 'Jane Doe',
+        username: 'Janey',
+        email: 'JaneyDoe@email.com'
+    },
+    {
+        id: 4,
+        name: 'Lebron James kid from vine',
+        username: 'LebronJames',
+        email: 'Iwentviralonce@lebronjhames.com'
+    },
+    {
+        id: 5,
+        name: 'Ulmo',
+        username: 'OceonGod',
+        email: 'Ocean@sea.com'
+    },
+]
